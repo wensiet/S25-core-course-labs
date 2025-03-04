ccba2bca60911845a942eb536b9213ff1d28e7a8
Iskander Shamsutdinov <wensietyt@gmail.com>
2025-03-04 19:08:26 +0300

    lab-12

--- app_python/main.py
+++ app_python/main.py
@@ -2,13 +2,14 @@ import uvicorn
 from fastapi import FastAPI
 
 from settings import APP_HOST, APP_PORT
-from routes import router
+from routes import time_router, visits_router
 from prometheus_fastapi_instrumentator import Instrumentator
 
 
 def main() -> None:
     app = FastAPI()
-    app.include_router(router)
+    app.include_router(time_router)
+    app.include_router(visits_router)
 
     Instrumentator().instrument(app).expose(app)
 
--- app_python/schemas.py
+++ app_python/schemas.py
@@ -5,3 +5,6 @@ from pydantic import BaseModel, Field
 
 class TimeResponse(BaseModel):
     time: datetime.datetime = Field(..., description="Текущее время по МСК")
+
+class VisitsResponse(BaseModel):
+    count: int
